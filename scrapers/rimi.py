1862107e84067868bc9cf1bf32bff0eab4be0337
bartkevicsjanis-ui <bartkevics.janis@gmail.com>
2026-01-20 18:26:03 +0200

    Update rimi.py

--- scrapers/rimi.py
+++ scrapers/rimi.py
@@ -1,29 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from scrapers.utils import normalize_name
 
 BASE_URL = "https://www.rimi.lv/e-veikals/lv/meklesana"
-
-HEADERS = {
-    "User-Agent": "Mozilla/5.0"
-}
+HEADERS = {"User-Agent": "Mozilla/5.0"}
 
 def search_rimi(product_name):
-    params = {"q": product_name}
-    r = requests.get(BASE_URL, params=params, headers=HEADERS, timeout=10)
+    r = requests.get(BASE_URL, params={"q": product_name}, headers=HEADERS, timeout=10)
     soup = BeautifulSoup(r.text, "lxml")
 
     products = []
     for item in soup.select("div.product-grid__item"):
         name = item.select_one("p.product-name")
         price = item.select_one("span.price__integer")
-
         if not name or not price:
             continue
 
         products.append({
             "shop": "Rimi",
             "name": name.text.strip(),
+            "norm_name": normalize_name(name.text),
             "price": float(price.text.replace(",", "."))
         })
-
     return products
